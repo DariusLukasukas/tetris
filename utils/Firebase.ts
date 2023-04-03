a943585a1f7c67be297553236edd9dc7b2a7ac8f
DariusLukasukas <64962012+DariusLukasukas@users.noreply.github.com>
2023-04-03 12:20:46 +0300

    Update Firebase.ts

--- utils/Firebase.ts
+++ utils/Firebase.ts
@@ -4,11 +4,14 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+const apiKey = process.env.NEXT_APP_FIREBASE_API_KEY;
+const authDomain = process.env.NEXT_APP_FIREBASE_AUTH_DOMAIN;
+const projectId = process.env.NEXT_APP_FIREBASE_PROJECT_ID;
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB9mLMU3w079Tn-Z5HIMXt5dAbWdOi5DCc",
-  authDomain: "tetris-2ebd9.firebaseapp.com",
-  projectId: "tetris-2ebd9",
+  apiKey,
+  authDomain,
+  projectId,
   storageBucket: "tetris-2ebd9.appspot.com",
   messagingSenderId: "1097388092406",
   appId: "1:1097388092406:web:6bcb22c6cc6316bd0335d7",
