f2d2c247483ab1def1d8aeb0685e5283c836f78e
DariusLukasukas <64962012+DariusLukasukas@users.noreply.github.com>
2023-04-03 12:58:15 +0300

    Update Firebase.ts

--- utils/Firebase.ts
+++ utils/Firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-const apiKey = process.env.NEXT_APP_FIREBASE_API_KEY;
-const authDomain = process.env.NEXT_APP_FIREBASE_AUTH_DOMAIN;
-const projectId = process.env.NEXT_APP_FIREBASE_PROJECT_ID;
-const storageBucket = process.env.NEXT_APP_FIREBASE_STORAGE_BUCKET;
-const messagingSenderId = process.env.NEXT_APP_FIREBASE_MESSAGING_SENDER_ID;
-const appId = process.env.NEXT_APP_FIREBASE_APP_ID;
+const apiKey = process.env.NEXT_PUBLIC_FIREBASE_API_KEY;
+const authDomain = process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN;
+const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
+const storageBucket = process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET;
+const messagingSenderId = process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID;
+const appId = process.env.NEXT_PUBLIC_FIREBASE_APP_ID;
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
